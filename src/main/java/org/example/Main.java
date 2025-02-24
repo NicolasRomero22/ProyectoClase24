6d0aaab4bee00c0577e1f58985cafe47966915e3
Nicolas <romerestup@gmail.com>
2025-02-24 17:11:18 -0500

    se cambio al switch nuevo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -27,49 +27,46 @@ public class Main {
             valor = scanner.nextInt();
 
             switch (valor) {
-                case 1:
+                case 1-> {
                     alcancia = alcancia + 20;
                     System.out.println("Has añadido 20 pesos.");
                     moneda20 = moneda20+1;
-                    break;
-                case 2:
+                }
+                case 2-> {
                     alcancia = alcancia + 50;
                     System.out.println("Has añadido 50 pesos.");
                     moneda50 = moneda50+1;
-                    break;
-                case 3:
+                }
+                case 3-> {
                     alcancia = alcancia + 100;
                     System.out.println("Has añadido 100 pesos.");
-                    moneda100 = moneda100+1;
-                    break;
-                case 4:
+                    moneda100 = moneda100 + 1;
+                }
+                case 4-> {
                     alcancia = alcancia + 200;
                     System.out.println("Has añadido 200 pesos.");
-                    moneda200 = moneda200+1;
-                    break;
-                case 5:
+                    moneda200 = moneda200 + 1;
+                }
+                case 5-> {
                     alcancia = alcancia + 500;
                     System.out.println("Has añadido 500 pesos.");
-                    moneda500 = moneda500+1;
-                    break;
-                case 6:
+                    moneda500 = moneda500 + 1;
+                }
+                case 6-> {
                     System.out.println("Usted tiene en total: " + moneda20 + " monedas de 20.");
                     System.out.println("Usted tiene en total: " + moneda50 + " monedas de 50.");
                     System.out.println("Usted tiene en total: " + moneda100 + " monedas de 100.");
                     System.out.println("Usted tiene en total: " + moneda200 + " monedas de 200.");
                     System.out.println("Usted tiene en total: " + moneda500 + " monedas de 500.");
-
-                    break;
-                case 7:
+                }
+                case 7-> {
                     System.out.println("Usted tiene: " + alcancia + " pesos ahorrados.");
-                    break;
-                case 8:
+                }
+                case 8-> {
                     System.out.println("¡Ha roto su alcancía! En total ahorró: " + alcancia + " pesos.");
-                    break;
-                default:
-                    System.out.println("Ingrese un valor válido entre 1 y 8.");
-                    break;
-            }
+                }
+                default ->     System.out.println("Ingrese un valor válido entre 1 y 8.");
+            };
         } while (valor != 8);
 
 
